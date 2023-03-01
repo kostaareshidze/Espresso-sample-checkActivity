addf257caf37e574ee5181cb20cd62a264d1f470
Lasha Mosakhlishvili <lmosakhlishvili@tbcbank.com.ge>
2023-03-02 03:33:32 +0400

    add helper actions

--- app/src/androidTest/java/com/ghedamsisabri/ui_testing_for_beginner/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/ghedamsisabri/ui_testing_for_beginner/ExampleInstrumentedTest.kt
@@ -1,12 +1,21 @@
 package com.ghedamsisabri.ui_testing_for_beginner
 
+import android.view.View
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.ghedamsisabri.ui_testing_for_beginner.Helper.isTextOnScreen
+import com.ghedamsisabri.ui_testing_for_beginner.Helper.isViewDisplayed
+import com.ghedamsisabri.ui_testing_for_beginner.Helper.tap
+import org.hamcrest.Matcher
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,12 +23,28 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
+@LargeTest
 class ExampleInstrumentedTest {
+
+    @get:Rule var activityScenarioRule = activityScenarioRule<MainActivity>()
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.ghedamsisabri.ui_testing_for_beginner", appContext.packageName)
+    }
 
+    @Test
+    fun checkSecondPage() {
+        NextBtn.tap()
+        secondPageActivity.isViewDisplayed()
+
+        isTextOnScreen("SecondaryActivity")
+    }
+
+    companion object{
+        val NextBtn: Matcher<View> by lazy { withId(R.id.button_next_activity) }
+        val secondPageActivity: Matcher<View> by lazy { withId(R.id.activity_secondary_title) }
     }
 }
